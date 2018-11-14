37376505c08d927e4f085b32fe0752cbed0f14e9
sababa11 <adamtarasov@gmail.com>
2018-11-14 13:50:25 +0200

    onother commit

--- prepare_env.sh
+++ prepare_env.sh
@@ -2,14 +2,19 @@
 
 sudo mkdir -p /var/www/demo-app
 
-sudo cp -vfr * /var/www/demo-app
+sudo yum install git -y
+
+git clone https://github.com/sababa11/elb_exercise.git
+
+sudo cp -vfr elb_exercise/* /var/www/demo-app
 
 sudo yum install mod_wsgi -y
 
 sudo yum install httpd -y
 
-sudo cp demo-app.conf /etc/httpd/conf.d
+sudo cp elb_exercise/demo-app.conf /etc/httpd/conf.d
+
+# changing listening port from 80 to 8080 in /etc/httpd/conf/httpd.conf
+sudo sed -i 's/Listen 80/Listen 8080/g' /etc/httpd/conf/httpd.conf
 
 sudo systemctl restart httpd
-
-# please change listening port from 80 to 8080 in /etc/httpd/conf/httpd.conf
